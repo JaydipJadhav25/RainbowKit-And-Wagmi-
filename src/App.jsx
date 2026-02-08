4aeb698f313091dfa240c9ae5a3b162f29e814be
JaydipJadhav25 <jaydipdjadhav@gmail.com>
2026-02-08 12:24:52 +0530

    init project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <ConnectButton/>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,45 @@
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+  darkTheme
+} from '@rainbow-me/rainbowkit';
+
+import { WagmiProvider } from 'wagmi';
+import { sepolia } from 'wagmi/chains';
+import {
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+
+//config
+
+const config = getDefaultConfig({
+  appName: 'DApp',
+  projectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID,
+  chains: [sepolia],
+  ssr: true, // If your dApp uses server side rendering (SSR)
+});
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById('root')).render(
+     <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider 
+        modalSize='compact'
+        theme={darkTheme({
+        accentColor: '#7b3fe4',
+        accentColorForeground: 'white',
+        borderRadius: 'medium',
+      })}
+        >
+           <App/>
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+)
